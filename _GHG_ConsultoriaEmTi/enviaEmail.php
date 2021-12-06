8b1de092b59efb964cf2f489d49c76d504559ad1
guitechnologic <39844853+guitechnologic@users.noreply.github.com>
2021-12-06 15:41:36 -0300

    Atualização MetaTags and CV GHG

--- _GHG_ConsultoriaEmTi/enviaEmail.php
+++ _GHG_ConsultoriaEmTi/enviaEmail.php
@@ -1,28 +1,28 @@
 <?php
 
-if(isset($_POST['Email']) and !empty($_POST['Email'])){
+if(isset($_POST(['Email']) && !empty($_POST(['Email']))))
 
-$nome = addslashes($_POST['Name']);
-$Email = addslashes($_POST['Email']);
-$Subject = addslashes($_POST['Subject']);
-$Message = addslashes($_POST['Message']);
+$nome = addslashes($_POST(['Name']));
+$Email = addslashes($_POST(['Email']));
+$Subject = addslashes($_POST(['Subject']));
+$Message = addslashes($_POST(['Message']));
 
 $to = "gui.technologic@gmail.com";
 $subject = "Contato";
-$body = "Nome: ".$nome."\n".
-        "Email: ".$Email."\n".
-        "Subject: ".$Subject."\n".
+$body = "Nome: ".$nome."\n"
+        "Email: ".$Email."\n"
+        "Subject: ".$Subject."\n"
         "Message: ".$Message;
-$header = "From:gui.technologic@gmail.com"."\r\n".
-        "Reply-to: ".$Email."\e\n".
-        "X=Mailer:PHP/".phpversion();
+$header = "From:gui.technologic@gmail.com"."\r\n"
+        ."Reply-to: ".$Email."\e\n"
+        ."X=Mailer:PHP/".phpversion();
 
 if(mail($to, $subject, $body, $header)){
     echo("email enviado com sucesso");
 }else{
     echo("email nãoenviado com sucesso");
 }
-}
+
 
 
 ?>
